4b316542100db44ee4fa28a0db2ed26ab85c37ab
Olivier Genel <zotri@hotmail.com>
2022-11-22 21:41:57 +0100

    added validations to elf and user models

--- app/models/elf.rb
+++ app/models/elf.rb
@@ -1,5 +1,12 @@
 class Elf < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :speciality, presence: true
+  validates :powers, presence: true
+  validates :location, presence: true
+  validates :price_per_day, numericality: { only_integer: true, greater_than: 0 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :elves
-  has_many :bookings
+  has_many :elves, dependent: :destroy
+  has_many :bookings, dependent: :destroy
   has_many :bookings, through: :elves
+
+  validates :name, presence: true
 end
