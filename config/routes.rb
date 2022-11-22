c1f48ac1e9803886789e04d71ead3ac4e5f36a0a
Olivier Genel <zotri@hotmail.com>
2022-11-22 21:04:41 +0100

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  get 'bookings/index'
-  get 'bookings/create'
-  get 'elves/index'
-  get 'elves/show'
-  get 'elves/new'
-  get 'elves/create'
   devise_for :users
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Root route to homepage
+  root to: "pages#home"
+
+  # App routes
+  resources :elves, only: %i[index new create show] do
+    resources :bookings, only: %i[create]
+  end
+
+  namespace :owner do
+    resources :bookings, only: %i[index]
+    resources :elves, only: %i[index]
+  end
 end
