b2998c8e721e7e75b1f21e6c6dcc736ab7c765c1
Olivier Genel <zotri@hotmail.com>
2022-11-24 16:59:44 +0100

    created owner/bookings controller and associated routes and views

---
+++ test/controllers/owner/bookings_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class Owner::BookingsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get owner_bookings_index_url
+    assert_response :success
+  end
+
+  test "should get accept" do
+    get owner_bookings_accept_url
+    assert_response :success
+  end
+
+  test "should get decline" do
+    get owner_bookings_decline_url
+    assert_response :success
+  end
+end
