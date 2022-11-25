b273ab3a27004cbdfdf8007d6b352bb687bde960
Olivier Genel <zotri@hotmail.com>
2022-11-25 15:08:37 +0100

    accept decline backend done

--- app/controllers/owner/bookings_controller.rb
+++ app/controllers/owner/bookings_controller.rb
@@ -1,15 +1,27 @@
 class Owner::BookingsController < ApplicationController
+  # before_action :set_booking, only: %i[accept decline]
   def index
     # @bookings are bookings AS RENTER
     @bookings = current_user.bookings
     @elves = current_user.elves
+    # @reservations = Booking.includes(:elf).where(elf: {user: current_user})
+  end
+
+  def accept
+    @booking = Booking.find(params[:id])
+    @booking.update(status: 'settled')
+    redirect_to owner_bookings_path
+  end
+
+  def reject
+    @booking = Booking.find(params[:id])
+    @booking.update(status: 'declined')
+    redirect_to owner_bookings_path
   end
 
   private
 
-  def accept
-  end
-
-  def decline
+  def set_booking
+    @booking = Booking.find(params[:id])
   end
 end
